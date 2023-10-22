7524a77ea96b1a207a3f9b8d380196e5054c1b7f
Ibrahim Mohammedi <ibrahimod77@gmail.com>
2023-10-22 14:11:13 +0100

    created the models file

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,9 +1,69 @@
 package models
 
+import "time"
+
 // Reservation holds reservation data
 type Reservation struct {
 	FirstName string
 	LastName  string
 	Email     string
 	Phone     string
+}
+
+type Users struct {
+	ID          int       `json:"id"`
+	FirstName   string    `json:"first_name"`
+	LastName    string    `json:"last_name"`
+	Email       string    `json:"email"`
+	Password    string    `json:"password"`
+	AccessLevel int       `json:"access_level"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
+
+// Room is the room model
+type Room struct {
+	ID        int
+	RoomName  string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+// Restrictions is the restriction model
+type Restrictions struct {
+	ID              int
+	RestrictionName string
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
+}
+
+// Reservations is the reservation model
+type Reservations struct {
+	ID        int
+	FirstName string
+	LastName  string
+	Email     string
+	Phone     string
+	StartDate time.Time
+	EndDate   time.Time
+	RoomID    int
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Room      Room
+	Processed int
+}
+
+// RoomRestrictions is the room restriction model
+type RoomRestrictions struct {
+	ID            int
+	StartDate     time.Time
+	EndDate       time.Time
+	RoomID        int
+	ReservationID int
+	RestrictionID int
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+	Room          Room
+	Reservation   Reservations
+	Restriction   Restrictions
 }
