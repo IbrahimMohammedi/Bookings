6062194f3c6dfb2f713226505a01a687229bee17
Ibrahim Mohammedi <ibrahimod77@gmail.com>
2023-11-06 12:15:35 +0100

    get user, update user, auth funcs

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -13,4 +13,7 @@ type DatabaseRepo interface {
 	InsertRoomRestriction(r models.RoomRestriction) error
 	SearchAvailabilityByDatesByRoomID(start, end time.Time, roomID int) (int, error)
 	SearchAvailabilityForAllRooms(start, end time.Time) ([]models.Room, error)
+	GetUserByID(id int) (models.User, error)
+	UpdateUser(u models.User) error
+	Auth(email, testPassword string) (int, string, error)
 }
